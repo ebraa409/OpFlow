b478eb5ce7ad0ede09aeb502563bc7c6ff30b6d8
ebraa <ebrahimsali99@gmail.com>
2023-11-11 13:48:41 +0300

    create task model

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from .models import Profile
 from .models import Workspace
+from .models import Task
 # Register your models here.
 
 admin.site.register(Profile)
 admin.site.register(Workspace)
+admin.site.register(Task)
--- main_app/models.py
+++ main_app/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth.models import auth, User
 from django.urls import reverse
 # Create your models here.
 
+STATUS = (
+  ('NS','Not Started'),
+  ('IP', 'In Progress'),
+  ('S', 'Stuck'),
+  ('PF', 'Pending Feedback'),
+  ('C', 'Completed')
+)
+
 class Profile(models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
   first_name = models.CharField(max_length=20)
@@ -20,3 +28,12 @@ class Workspace(models.Model):
     return reverse('detail', kwargs={'workspace_id': self.id})
 
 
+class Task(models.Model):
+  name = models.CharField(max_length=50)
+  description = models.TextField(max_length=300)
+  due_date = models.DateField()
+  status = models.CharField(max_length=100, choices=STATUS, default=STATUS[0][0])
+  workspace = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.workspace.name} - {self.get_status_display()} on {self.date}'
